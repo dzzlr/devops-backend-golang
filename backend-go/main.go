5893e116bb36f990e111d33f96b676ebd4a62259
dzzlr <inigithudazzler@gmail.com>
2023-10-28 05:46:48 +0700

    Edit: main.go

--- backend-go/main.go
+++ backend-go/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/dzzlr/devops-backend-golang/backend-go/models"
-	"github.com/dzzlr/devops-backend-golang/controllers/backend-go/productcontroller"
+	"github.com/dzzlr/devops-backend-golang/backend-go/controllers/productcontroller"
 )
 
 func main() {
