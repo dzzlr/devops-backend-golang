be5e67e0be78766cd34aa171b0346a5690be7756
dzzlr <inigithudazzler@gmail.com>
2023-10-28 16:24:43 +0700

    Refactor: Infrastructure

--- backend-go/infrastructure/db.go
+++ backend-go/infrastructure/db.go
@@ -19,15 +19,14 @@ func NewDatabase() Database {
     HOST := os.Getenv("DB_HOST")
     DBNAME := os.Getenv("DB_NAME")
 
-    URL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, 
-    HOST, DBNAME)
+    URL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=True&loc=Local", USER, PASS, HOST, DBNAME)
     fmt.Println(URL)
     db, err := gorm.Open(mysql.Open(URL))
 
     if err != nil {
         panic("Failed to connect to database!")
-
     }
+    
     fmt.Println("Database connection established")
     return Database{
         DB: db,
--- backend-go/infrastructure/routes.go
+++ backend-go/infrastructure/routes.go
@@ -12,14 +12,13 @@ type GinRouter struct {
 
 //NewGinRouter all the routes are defined here
 func NewGinRouter() GinRouter {
-
     httpRouter := gin.Default()
 
     httpRouter.GET("/", func(c *gin.Context) {
         c.JSON(http.StatusOK, gin.H{"data": "Up and Running..."})
     })
+
     return GinRouter{
         Gin: httpRouter,
     }
-
 }
