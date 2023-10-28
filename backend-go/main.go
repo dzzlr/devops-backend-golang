12982d1ab0019df8c9100682b7c5bede2e5a501e
dzzlr <inigithudazzler@gmail.com>
2023-10-28 16:21:10 +0700

    Init: Simple Rest API with Golang

--- backend-go/main.go
+++ backend-go/main.go
@@ -1,20 +1,29 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
-	"github.com/dzzlr/devops-backend-golang/backend-go/models"
-	"github.com/dzzlr/devops-backend-golang/backend-go/controllers/productcontroller"
+	"github.com/dzzlr/devops-backend-golang/backend-go/api/controller"
+    "github.com/dzzlr/devops-backend-golang/backend-go/api/repository"
+    "github.com/dzzlr/devops-backend-golang/backend-go/api/routes"
+    "github.com/dzzlr/devops-backend-golang/backend-go/api/service"
+    "github.com/dzzlr/devops-backend-golang/backend-go/infrastructure"
+    "github.com/dzzlr/devops-backend-golang/backend-go/models"
 )
 
+func init() {
+    infrastructure.LoadEnv()
+}
+
 func main() {
-	r := gin.Default()
-	models.ConnectDatabase()
+	router := infrastructure.NewGinRouter() //router has been initialized and configured
+    db := infrastructure.NewDatabase() // databse has been initialized and configured
 
-	r.GET("/api/products", productcontroller.Index)
-	r.GET("/api/products/:id", productcontroller.Show)
-	r.POST("/api/products", productcontroller.Create)
-	r.PUT("/api/products/:id", productcontroller.Update)
-	r.DELETE("/api/products", productcontroller.Delete)
+    postRepository := repository.NewPostRepository(db) // repository are being setup
+    postService := service.NewPostService(postRepository) // service are being setup
+    postController := controller.NewPostController(postService) // controller are being set up
+	
+    postRoute := routes.NewPostRoute(postController, router) // post routes are initialized
+    postRoute.Setup() // post routes are being setup
 
-	r.Run()
+    db.DB.AutoMigrate(&models.Post{}) // migrating Post model to datbase table
+    router.Gin.Run(":8000") //server started on 8000 port
 }
